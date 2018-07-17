f38147cc24398cafeaac74dc5d82e9647d76d76f
Dan Nolan <dan@proxima.io>
2018-07-17 20:38:57 +1000

    Nicking some stuff to make logging easier

--- main.go
+++ main.go
@@ -1,17 +1,27 @@
 package main
 
 import (
+	"context"
+	"fmt"
 	"log"
 	"net/http"
 	"os"
+	"time"
 
 	"github.com/dannolan/webparrot/handlers"
+)
+
+type key int
+
+const (
+	requestIDKey key = 0
 )
 
 func main() {
 	logger := log.New(os.Stdout, "webparrot:", log.LstdFlags)
 	logger.Println("Loading...")
 	listenAddr := ":5000"
+
 	router := http.NewServeMux()
 	router.Handle("/healthz", handlers.HealthHandler{
 		Logger: logger,
@@ -20,5 +30,49 @@ func main() {
 		Logger: logger,
 	})
 
-	log.Fatal(http.ListenAndServe(listenAddr, router))
+	nextRequestID := func() string {
+		return fmt.Sprintf("%d", time.Now().UnixNano())
+	}
+
+	server := &http.Server{
+		Addr:         listenAddr,
+		Handler:      tracing(nextRequestID)(logging(logger)(router)),
+		ErrorLog:     logger,
+		ReadTimeout:  5 * time.Second,
+		WriteTimeout: 10 * time.Second,
+		IdleTimeout:  15 * time.Second,
+	}
+
+	log.Fatal(server.ListenAndServe())
+}
+
+//SHAMELESSLY STOLEN FROM https://gist.github.com/enricofoltran/10b4a980cd07cb02836f70a4ab3e72d7
+
+func logging(logger *log.Logger) func(http.Handler) http.Handler {
+	return func(next http.Handler) http.Handler {
+		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			defer func() {
+				requestID, ok := r.Context().Value(requestIDKey).(string)
+				if !ok {
+					requestID = "unknown"
+				}
+				logger.Println(requestID, r.Method, r.URL.Path, r.RemoteAddr, r.UserAgent())
+			}()
+			next.ServeHTTP(w, r)
+		})
+	}
+}
+
+func tracing(nextRequestID func() string) func(http.Handler) http.Handler {
+	return func(next http.Handler) http.Handler {
+		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			requestID := r.Header.Get("X-Request-Id")
+			if requestID == "" {
+				requestID = nextRequestID()
+			}
+			ctx := context.WithValue(r.Context(), requestIDKey, requestID)
+			w.Header().Set("X-Request-Id", requestID)
+			next.ServeHTTP(w, r.WithContext(ctx))
+		})
+	}
 }
