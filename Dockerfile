017ebd95743ed938ef1ba0c9dc0fdf00cba00183
Dan Nolan <dan@proxima.io>
2018-07-24 19:52:13 +1000

    Docker file updates, using dep and using letsencrypt

--- Dockerfile
+++ Dockerfile
@@ -2,9 +2,11 @@ FROM golang:1.10 AS builder
 COPY . "/go/src/github.com/dannolan/webparrot"
 WORKDIR "/go/src/github.com/dannolan/webparrot"
 RUN CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build  -o /webparrot
-EXPOSE 5000
 
 FROM scratch
 COPY --from=builder /webparrot .
 EXPOSE 5000
+EXPOSE 443
+EXPOSE 80
 CMD ["./webparrot"]
+VOLUME ["/certs"]  
--- main.go
+++ main.go
@@ -2,11 +2,14 @@ package main
 
 import (
 	"context"
+	"crypto/tls"
 	"fmt"
 	"log"
 	"net/http"
 	"os"
 	"time"
+
+	"golang.org/x/crypto/acme/autocert"
 
 	"github.com/dannolan/webparrot/handlers"
 )
@@ -17,10 +20,36 @@ const (
 	requestIDKey key = 0
 )
 
+var defaultAddress = ":5000"
+
+func getHostingAddress() string {
+	address := os.Getenv("HOST_ADDRESS")
+	if len(address) != 0 {
+		return address
+	}
+	return defaultAddress
+}
+
+func runningInProduction() bool {
+	inProd := os.Getenv("PRODUCTION_ENV")
+	if len(inProd) > 0 {
+		return true
+	}
+	return false
+}
+
+func hostDomain() string {
+	hostDomain := os.Getenv("PRODUCTION_DOMAIN")
+	if len(hostDomain) > 0 {
+		return hostDomain
+	}
+	return "example.com"
+}
+
 func main() {
 	logger := log.New(os.Stdout, "webparrot:", log.LstdFlags)
 	logger.Println("Loading...")
-	listenAddr := ":5000"
+	listenAddr := getHostingAddress()
 
 	router := http.NewServeMux()
 	router.Handle("/healthz", handlers.HealthHandler{
@@ -34,16 +63,42 @@ func main() {
 		return fmt.Sprintf("%d", time.Now().UnixNano())
 	}
 
-	server := &http.Server{
-		Addr:         listenAddr,
-		Handler:      tracing(nextRequestID)(logging(logger)(router)),
-		ErrorLog:     logger,
-		ReadTimeout:  5 * time.Second,
-		WriteTimeout: 10 * time.Second,
-		IdleTimeout:  15 * time.Second,
+	var server *http.Server
+
+	certManager := autocert.Manager{
+		Prompt:     autocert.AcceptTOS,
+		HostPolicy: autocert.HostWhitelist(hostDomain()), //Your domain here
+		Cache:      autocert.DirCache("certs"),           //Folder for storing certificates
 	}
 
-	log.Fatal(server.ListenAndServe())
+	if runningInProduction() {
+		server = &http.Server{
+			Addr:         ":https",
+			Handler:      tracing(nextRequestID)(logging(logger)(router)),
+			ErrorLog:     logger,
+			ReadTimeout:  5 * time.Second,
+			WriteTimeout: 10 * time.Second,
+			IdleTimeout:  15 * time.Second,
+			TLSConfig: &tls.Config{
+				GetCertificate: certManager.GetCertificate,
+			},
+		}
+		go http.ListenAndServe(":http", certManager.HTTPHandler(nil))
+		log.Fatal(server.ListenAndServeTLS("", ""))
+
+	} else {
+		server = &http.Server{
+			Addr:         listenAddr,
+			Handler:      tracing(nextRequestID)(logging(logger)(router)),
+			ErrorLog:     logger,
+			ReadTimeout:  5 * time.Second,
+			WriteTimeout: 10 * time.Second,
+			IdleTimeout:  15 * time.Second,
+		}
+		log.Fatal(server.ListenAndServe())
+
+	}
+
 }
 
 //SHAMELESSLY STOLEN FROM https://gist.github.com/enricofoltran/10b4a980cd07cb02836f70a4ab3e72d7
